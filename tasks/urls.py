c7b8958fd19da002d98d0c93d82b653f0e36c42c
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-09-02 15:01:37 +0300

    feat: setup index view, project list dashboard, and URL routing

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import HomeView, ProjectListView
+
+urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
+    path("projects/", ProjectListView.as_view(), name="project-list"),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.views.generic import ListView, TemplateView
+
+from .models import Project
+
+
+class HomeView(TemplateView):
+    template_name = "index.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("project-list")
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ProjectListView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = "tasks/project_list.html"
+    context_object_name = "projects"
+
+    def get_queryset(self):
+        return Project.objects.filter(user=self.request.user).prefetch_related("tasks")
