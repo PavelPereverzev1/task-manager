54ff146d7877a67825f94ab02c67f2fb08e51730
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-09-02 16:15:38 +0300

    feat: add custom bootstrap templates for allauth login and signup

--- config/settings.py
+++ config/settings.py
@@ -94,3 +94,6 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "project-list"
+LOGOUT_REDIRECT_URL = "home"
